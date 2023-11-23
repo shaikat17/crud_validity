407ca32a4ed6acaba82c67e0cc7073a9748417cc
shaikat17 <shaikatpal56@gmail.com>
2023-11-23 23:44:36 +0600

    Product Models Creaeated.

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,25 +1,29 @@
 export type FullName = {
-firstName: "string",
-lastName: "string"
+firstName: string,
+lastName: string
 }
 
 export type Address = {
-    street: "string",
-    city: "string",
-    country: "string"
+    street: string,
+    city: string,
+    country: string
+}
+
+export type ProductDetails = {
+    productName: string,
+    price: number,
+    quantity: number
 }
 
 export type User = {
-    userId: "number",
-    username: "string",
-    password: "string",
+    userId: number,
+    username: string,
+    password: string,
     fullName: FullName,
-    age: "number",
-    email: "string",
-    isActive: "boolean",
-    hobbies: [
-        "string",
-        "string"
-    ],
-    address: Address
+    age: number,
+    email: string,
+    isActive: boolean,
+    hobbies: string[],
+    address: Address,
+    orders: ProductDetails[]
 }
---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,34 @@
+import { Schema, model, connect } from "mongoose";
+import { Address, FullName, ProductDetails, User } from "./user.interface";
+
+const FullNameSchema = new Schema<FullName>({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+  });
+  
+  const AddressSchema = new Schema<Address>({
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+  });
+
+  const ProductSchema = new Schema<ProductDetails>({
+  productName: {type: String, required: true},
+  price: {type: Number, required: true},
+  quantity: {type: Number, required: true}
+});
+  
+  const UserSchema: Schema = new Schema({
+    userId: { type: Number, required: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    fullName: { type: FullNameSchema, required: true },
+    age: { type: Number, required: true },
+    email: { type: String, required: true },
+    isActive: { type: Boolean, required: true },
+    hobbies: { type: [String], required: true },
+    address: { type: AddressSchema, required: true },
+    orders: {type: [ProductSchema]}
+  });
+  
+  export const UserModel = model<User>('User', UserSchema);
