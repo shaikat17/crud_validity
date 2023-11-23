2c22ad7abf0de17cfcf8134761edeabbc52b36d4
shaikat17 <shaikatpal56@gmail.com>
2023-11-23 22:50:32 +0600

    user interface created.

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,25 @@
+export type FullName = {
+firstName: "string",
+lastName: "string"
+}
+
+export type Address = {
+    street: "string",
+    city: "string",
+    country: "string"
+}
+
+export type User = {
+    userId: "number",
+    username: "string",
+    password: "string",
+    fullName: FullName,
+    age: "number",
+    email: "string",
+    isActive: "boolean",
+    hobbies: [
+        "string",
+        "string"
+    ],
+    address: Address
+}
